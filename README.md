3fec09eac4bc78297c8e509b009ca95ab6394fdc
bencrowder <ben.crowder@gmail.com>
2013-03-26 14:45:23 -0600

    Got templates hooked in

---
+++ README.md
@@ -0,0 +1,10 @@
+## Vinci
+
+Web-based notebook.
+
+### Installation
+
+1. Create a virtualenv (we recommend virtualenvwrapper).
+2. Activate your virtualenv.
+3. Run `pip install -r requirements.txt`.
+4. Copy `config_sample.py` to `config.py` and edit.
--- web.py
+++ web.py
@@ -1,20 +1,43 @@
-from flask import Flask, request
-app = Flask(__name__)
+# Vinci web app
+
+from flask import Flask, request, render_template, url_for, send_from_directory
 import config
+import os
+import vinci
+
+app = Flask(__name__)
+
+@app.route('/favicon.ico/')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/<notebook_slug>/')
 @app.route('/<notebook_slug>/type:<response_type>/')
 def display_entries(notebook_slug, response_type='html'):
-    return " ".join([notebook_slug, response_type])
+    # Choose template (TODO: refactor)
+    if response_type == 'html':
+        template = 'list.html'
+    elif response_type == 'json':
+        template = 'list-json.html'
 
-@app.route('/<notebook>/add')
+    notebook = vinci.get_notebook(notebook_slug)
+    entries = vinci.get_entries(notebook_slug)
+
+    return render_template(template, notebook=notebook, entries=entries)
+
+@app.route('/<notebook_slug>/add')
 def add_entry(notebook_slug):
     content = request.args.get('content')
+    date = request.args.get('date')
 
+    if vinci.add_entry(content=content, notebook_slug=notebook_slug, date=date):
+        return '1'
+    else:
+        return '0'
 
 @app.route('/')
-def test():
-    return config.database_file
+def index():
+    return 'hello'
 
 #@app.route('/<notebook>/<entry_id>')
 #@app.route('/search/<query>')

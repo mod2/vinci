123e746e7960ac52be1d52dd5c952d0d9cc93208
chadgh <chadgh@gmail.com>
2013-03-26 13:46:58 -0600

    updated web and added config sample file

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 app = Flask(__name__)
+import config
 
 @app.route('/<notebook_slug>/')
 @app.route('/<notebook_slug>/type:<response_type>/')
@@ -10,10 +11,15 @@ def display_entries(notebook_slug, response_type='html'):
 def add_entry(notebook_slug):
     content = request.args.get('content')
 
+
+@app.route('/')
+def test():
+    return config.database_file
+
 #@app.route('/<notebook>/<entry_id>')
 #@app.route('/search/<query>')
 #@app.route('/<notebook>/search/<query>')
 
 if __name__ == '__main__':
-    app.debug = True
+    app.debug = config.debug
     app.run()

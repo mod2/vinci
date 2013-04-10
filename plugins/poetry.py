fa9a40b6f78df1f07ad5eff937f3ce2b56fd97b4
bencrowder <ben.crowder@gmail.com>
2013-04-10 14:57:25 -0600

    Added docs for poetry plugin

--- plugins/poetry.py
+++ plugins/poetry.py
@@ -1,4 +1,22 @@
 # Poetry plugin
+#
+# Usage:
+#
+#    {poetry
+#    &rsquo;Twas brillig, and the slithy toves
+#      Did gyre and gimble in the wabe:
+#    All mimsy were the borogoves,
+#      And the mome raths outgrabe.
+#
+#    "Beware the Jabberwock, my son!
+#      The jaws that bite, the claws that catch!
+#    Beware the Jubjub bird, and shun
+#      The frumious Bandersnatch!"
+#    }
+#
+# - Blank line for new stanza
+# - Two spaces to indent (or four, or six)
+# - Quotes get curled, but Markdown isn't applied (use HTML tags)
 
 import re
 

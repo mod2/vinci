97c052d5e7f1825fc41f8ffd4f3ede564338d6dd
bencrowder <ben.crowder@gmail.com>
2014-11-05 09:35:49 -0700

    Updated tab code

--- static/js/vinci.js
+++ static/js/vinci.js
@@ -423,23 +423,16 @@ $(document).ready(function() {
 	});
 
 	$(document).on("keydown", "textarea#add, .editbox textarea", function(e) {
-		if (e.keyCode === 9) { // tab was pressed
-			// get caret position/selection
+		if (e.keyCode === 9) {
 			var start = this.selectionStart;
 			var end = this.selectionEnd;
 
 			var $this = $(this);
 			var value = $this.val();
 
-			// set textarea value to: text before caret + tab + text after caret
-			$this.val(value.substring(0, start)
-						+ "\t"
-						+ value.substring(end));
+			$this.val(value.substring(0, start) + "\t" + value.substring(end));
 
-			// put caret at right position again (add one for the tab)
 			this.selectionStart = this.selectionEnd = start + 1;
-
-			// prevent the focus lose
 			e.preventDefault();
 		}
 	});

3799bad56aa79e826c3fab77fa041e6015c612f6
bencrowder <ben.crowder@gmail.com>
2015-06-19 11:34:59 -0600

    Adds mgmt cmd to move Today list items to Inbox

---
+++ vinci/management/commands/movetoday.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from vinci.models import Notebook, List, Card
+from django.contrib.auth.models import User
+
+import sqlite3
+import datetime
+import json
+
+class Command(BaseCommand):
+    help = 'For each Today list, moves all items to the corresponding Inbox list'
+
+    def handle(self, *args, **options):
+        try:
+            today_lists = List.objects.filter(slug='today')
+
+            for list in today_lists:
+                try:
+                    inbox_list = List.objects.get(slug='inbox', notebook=list.notebook)
+
+                    num_cards = inbox_list.cards.all().count()
+                    if num_cards > 0:
+                        # Reorder existing cards so the new ones show up in order
+                        # (If there's just one, order=0 will put it at the top)
+                        list_cards = Card.objects.filter(list=inbox_list).order_by('order')
+                        for card in list_cards:
+                            card.order += num_cards
+                            card.save()
+
+                    # Move all cards from list to inbox_list
+                    order = 0
+                    for card in list.cards.all():
+                        card.list = inbox_list
+                        card.order = order
+                        card.save()
+
+                        order += 1
+                except Exception as e:
+                    pass
+
+            print("Done! Don't forget to reindex")
+
+        except Exception as e:
+            print(e)
